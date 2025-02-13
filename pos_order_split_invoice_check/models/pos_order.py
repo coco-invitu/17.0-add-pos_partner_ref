29bab7ad3ba8351e58ec540312b29375f9243859
Vehiana Tepuhiarii <vehi@invitu.com>
2025-02-13 13:11:47 -1000

    [IMP] Set 'to_check' to True on split invoice creation

---
+++ pos_order_split_invoice_check/models/pos_order.py
@@ -0,0 +1,10 @@
+from odoo import models
+
+
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
+
+    def _create_splitting_invoice(self, move_vals):
+        new_move = super()._create_splitting_invoice(move_vals)
+        new_move.to_check = True
+        return new_move
